c1aca425e6652935649459cf60834914c98ae1fe
Tay J Solis <tayjsolis@gmail.com>
2018-11-27 09:40:16 -0800

    Functioning basic prototype with Modal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react'
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, Button} from 'react-native';
+
+class Todo extends Component{
+    onDeletePress = () =>{
+        return this.props.onDeletePress(this.props.dateSubmitted)
+    }
+
+    onDetailsPress =()=>{
+        return this.props.onDetailsPress(this.props.dateSubmitted)
+    }
+    render(){
+        return(
+            <View style={styles.todo}>
+                <View style={{flexDirection: 'row', alignItems:'center', width:'60%'}}>
+                <TouchableOpacity
+                    style={styles.delete}
+                    onPress={this.onDeletePress}
+                    >
+                        <Text
+                        style={styles.deleteText}>X</Text>
+                    </TouchableOpacity>
+                    <Text >
+                        {this.props.todoName}
+                    </Text>
+                </View>
+
+                <Button
+                onPress={this.onDetailsPress}
+                color='#be95ff'
+                title='Read More...'
+                />
+
+            </View>
+
+        );
+    }
 
 
-
-const Todo = (props) => {
-		return(
-			<View style={styles.todo}>
-				<Text > 
-					{props.todoName}
-				</Text>
-			</View>
-			
-    );
-    
 }
 
 
@@ -21,7 +45,31 @@ const styles = StyleSheet.create({
 		width: '100%',
 		padding: 10,
 		fontSize: 16,
-		backgroundColor: '#eee',
-	}
+        backgroundColor: '#eee',
+        marginBottom: 5,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingRight: 20
+    },
+    delete: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: 2,
+        width: 20,
+        height: 20,
+        borderRadius: 10,
+        fontSize: 18,
+        backgroundColor: '#333',
+        marginLeft: 20,
+        marginRight: 10,
+    },
+    deleteText:{
+        color: '#EEE',
+    },
+    details:{
+        marginRight: 20
+    }
 })
 export default Todo
---
+++ src/components/modals/TodoModal.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Modal, View, Text, StyleSheet, Button } from 'react-native'
+
+const TodoModal = (props) => (
+    <Modal animationType="slide">
+        <View
+          style={styles.container}>
+            <Text style={styles.title}>{props.selectedTodo.value}</Text>
+          <Text>Created  {timeAgo(props.selectedTodo.dateSubmitted)}</Text>
+          <View style={styles.buttons}>
+            <Button
+              title="Delete"
+              color='#be95ff'
+              onPress={props.onDeleteModal}
+              />
+            <Button
+              title="Close"
+              color= '#be95ff'
+            onPress={props.onModalClose}/>
+          </View>
+
+        </View>
+
+
+    </Modal>
+)
+
+const styles = StyleSheet.create({
+  container:{
+      flex:1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    paddingTop: 30
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  buttons:{
+    flexDirection: 'row'
+  }
+})
+
+const timeAgo =(past) =>{
+    let today = Date.now();
+    let time = Math.floor((today - past) / (1000 * 60 * 60));
+    if (time < 1) {
+      time = Math.floor((today - past) / (1000 * 60));
+      return `${time} minutes ago`;
+    }
+    if (time < 24) {
+      return `${time} hours ago`;
+    } else {
+      time = Math.floor(time / 24);
+      if (time === 1) {
+        return `${time} day ago`;
+      } else {
+        return `${time} days ago`;
+      }
+    }
+  }
+export default TodoModal
